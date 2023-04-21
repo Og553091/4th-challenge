753fa15ce8858099d39410b1ed9f8af6744d213c
oscar garcia <og553091@gmail.com>
2023-04-20 20:30:43 -0500

    finished quiz

---
+++ questions.js
@@ -0,0 +1,70 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Where do you put the link for the JavScript file in HTML?",
+    answer: "In the Head tag",
+    options: [
+      "In the body tag",
+      "In the footer tag",
+      "In the Head tag",
+      "In the Main tag"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How do you call a function in JavaScript?",
+    answer: "By typing the function name follwed by parenthesis",
+    options: [
+      "By creating a variable and setting it equal to the function name",
+      "By creating an array and setting it equal to the function",
+      "By using a query selector to select the function",
+      "By typing the function name follwed by parenthesis"
+    ]
+  },
+    {
+    numb: 3,
+    question: "what kinds of values can an array store?",
+    answer: "All of these",
+    options: [
+      "Numbers",
+      "Booleans",
+      "Strings",
+      "All of these"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Whst do you use to make an api request ?",
+    answer: "By using Fetch",
+    options: [
+      "By calling a function",
+      "By creating a variable",
+      "By using Fetch",
+      "By using console.log"
+    ]
+  },
+    {
+    numb: 5,
+    question: "what does an object contain?",
+    answer: "A key and a value",
+    options: [
+      " A variable",
+      "A function",
+      "A key and a value",
+      "An if statement"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
